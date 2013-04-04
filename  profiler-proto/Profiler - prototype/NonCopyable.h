c4a116f9ffafd5a98c0bc09ba029eb1714fc9abc
deffax <deffax@3c8bf714-5292-ed19-5394-fc0754240f3e>
2013-04-04 11:04:42 +0000

    implementazione CallstackProfiler

---  profiler-proto/Profiler - prototype/NonCopyable.h
+++  profiler-proto/Profiler - prototype/NonCopyable.h
@@ -1,1 +1,10 @@
 #pragma once
+class NonCopyable
+{
+  protected:
+    NonCopyable () {}
+    ~NonCopyable () throw() {} /// Protected non-virtual destructor
+  private: 
+    NonCopyable (const NonCopyable &);
+    NonCopyable & operator = (const NonCopyable &);
+};
