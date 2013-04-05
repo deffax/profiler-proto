b9cf10225ee092522bc9da0aace58aa697cd3a17
deffax <deffax@3c8bf714-5292-ed19-5394-fc0754240f3e>
2013-04-05 12:08:51 +0000

    implementazione di MemoryProfilerNode

--- trunk/ profiler-proto/Profiler - prototype/main.cpp
+++ trunk/ profiler-proto/Profiler - prototype/main.cpp
@@ -1,4 +1,22 @@
+#include "MemoryProfiler.h"
+
+typedef MemoryProfiler::Scope Scope;
+
+void functionB()
+{
+	Scope s("functionB");
+}
+
+
+void functionA()
+{
+	Scope s("functionA");
+	functionB();
+}
+
+
 int main()
 {
+	functionA();
 	return 0;
 }
